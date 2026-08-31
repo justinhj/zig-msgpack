74f8d460ea64f756b452cd988f07d3464d057902
Justin Heyes-Jones <justinhj@gmail.com>
2026-08-30 21:03:38 -0700

    Initial stages of msgpack parse

--- src/main.zig
+++ src/main.zig
@@ -31,41 +31,46 @@ pub fn main(init: std.process.Init) !void {
     try stdout_writer.flush(); // Don't forget to flush!
 }
 
-test "simple test" {
+pub const MsgPackType = enum {
+    array,
+    integer,
+    string,
+};
+
+pub const MsgPackObject = union(MsgPackType) {
+    array: []*MsgPackObject,
+    integer: i64,
+    string: []u8,
+};
+
+pub const MsgPackError = error {
+    incomplete, // If you run out of bytes while parsing
+};
+
+// Unpack the msgpack data to 
+pub fn unpack(allocator: std.mem.Allocator, input: []const u8) !MsgPackObject {
+    // 94 00 = 1001 0100 0000 0000 
+    const i :usize = 0;
+    if (input.len == 0) {
+        return MsgPackError.incomplete;
+    }
+
+    var obj: MsgPackObject = undefined;
+
+    // maximum number of elements of an Array object is `(2^32)-1`
+    // Check for fixarray (up to 15 elements)
+    if (input[i] >= 0x94 and input[i] <= 0x9f) {
+        const array = try allocator.alloc(*MsgPackObject, input[i] - 0x90);
+        obj = MsgPackObject{ .array = array };
+    } 
+    return obj;
+} 
+
+test "unpack command" {
     const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const test_input = "\x94\x00\x01\xa9nvim_eval\x91\xa52 + 2";
+    // Output [0, 1, 'nvim_eval', ['2 + 2']]
+    const obj = try unpack(gpa, test_input);
+    try std.testing.expect(obj == .array);
 }
 
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
-}
